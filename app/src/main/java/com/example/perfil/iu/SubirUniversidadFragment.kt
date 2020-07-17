baa0698cc2921c952ac626420d47acdf4ce31571
FabianAlejandroTapiaJorquera <fabian_alejandro_tapia@hotmail.com>
2020-07-17 00:23:18 -0400

    Subida de imagen de la institución a cloud storage de Firebase

--- app/src/main/java/com/example/perfil/iu/SubirUniversidadFragment.kt
+++ app/src/main/java/com/example/perfil/iu/SubirUniversidadFragment.kt
@@ -1,18 +1,33 @@
 package com.example.perfil.iu
 
+import android.app.Activity.RESULT_OK
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.DialogFragment
+import com.bumptech.glide.Glide
 import com.example.perfil.R
 import com.example.perfil.Universidad
 import com.example.perfil.subirDatos.SubirUniversidad
 import com.example.perfil.subirDatos.SubirUniversidadBD
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.storage.FirebaseStorage
+import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.fragment_subir_universidad.*
+import java.util.*
+import kotlin.random.Random
 
 class SubirUniversidadFragment : DialogFragment() {
+
+    private val letras = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+    private val codigo by lazy { generarCodigo() }
+
+    private val GALLERY_PICK_INTENT by lazy { 1 }
 
     private lateinit var firebase: SubirUniversidadBD
     private lateinit var descartar: Button
@@ -20,6 +35,7 @@ class SubirUniversidadFragment : DialogFragment() {
     private lateinit var nombre: EditText
     private lateinit var direccion: EditText
     private lateinit var telefono: EditText
+    private lateinit var foto: CircleImageView
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return crearDialogo()
@@ -37,11 +53,19 @@ class SubirUniversidadFragment : DialogFragment() {
         nombre = view.findViewById(R.id.auNombre)
         direccion = view.findViewById(R.id.auDireccion)
         telefono = view.findViewById(R.id.auTelefono)
+        foto = view.findViewById(R.id.agFotoPerfil)
 
 
        funcionesBotones()
 
         return builder.create()
+    }
+
+    fun generarCodigo(): String{
+        var codigo = ""
+        for(i in 0..10)
+            codigo = codigo + letras[Random.nextInt(0, letras.size)]
+        return codigo
     }
 
     fun funcionesBotones(){
@@ -51,9 +75,26 @@ class SubirUniversidadFragment : DialogFragment() {
         }
 
         subir.setOnClickListener {
-            firebase = SubirUniversidadBD(SubirUniversidad(Universidad(nombre.text.toString(), "", nombre.text.toString().toLowerCase())))
+            firebase = SubirUniversidadBD(SubirUniversidad(Universidad(nombre.text.toString(), "", codigo)))
             firebase.subirUniversidadBD()
             dismiss()
+        }
+
+        foto.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, GALLERY_PICK_INTENT)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GALLERY_PICK_INTENT && resultCode == RESULT_OK) {
+            val uri = data?.data
+            val referencia = FirebaseStorage.getInstance().getReference().child("fotos").child(codigo).child(uri!!.lastPathSegment.toString())
+            referencia.putFile(uri!!)
         }
     }
 }
