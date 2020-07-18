d34e97e587460e882853686f5923d74018dce395
FabianAlejandroTapiaJorquera <fabian_alejandro_tapia@hotmail.com>
2020-07-18 16:33:47 -0400

    Implementación de funciones para ocultar el bottom navigation en fragments que no sean los de navegación

--- app/src/main/java/com/example/perfil/iu/NavegacionActivity.kt
+++ app/src/main/java/com/example/perfil/iu/NavegacionActivity.kt
@@ -2,6 +2,7 @@ package com.example.perfil.iu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.perfil.R
@@ -15,6 +16,24 @@ class NavegacionActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment)
         bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when(destination.id){
+                R.id.inicioFragment -> showBottomNav()
+                R.id.chatFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
+
+    }
+
+    private fun showBottomNav() {
+        bottomNavigationView.visibility = View.VISIBLE
+
+    }
+
+    private fun hideBottomNav() {
+        bottomNavigationView.visibility = View.GONE
 
     }
 
